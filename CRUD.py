f4e99b03e96935a90562e1d38bf39db030be5341
Anderson Frasao <anderfrasao52@gmail.com>
2023-11-22 22:55:45 -0300

    ediçao OK

--- CRUD.py
+++ CRUD.py
@@ -5,104 +5,103 @@ def converter(dicionario, tipo, entrada, acao):
             dicionario[tipo].append(int(entrada))
         elif acao == 3:
             index = dicionario[tipo].index(int(entrada))
-            dicionario[tipo]
-            return            
+            dicionario[tipo][index] = int(input("Digite o novo dado\n"))
         elif acao == 4:
             dicionario[tipo].remove(int(entrada))
     elif tipo == "float":
         if acao == 1:
             dicionario[tipo].append(float(entrada))
-        elif acao == 3:
-            return            
-        
+        elif acao == 3:        
+            index = dicionario[tipo].index(float(entrada))
+            dicionario[tipo][index] = float(input("Digite o novo dado\n"))
         elif acao == 4:
             dicionario[tipo].remove(float(entrada))
     elif tipo == "complex":
         if acao == 1:
             dicionario[tipo].append(complex(entrada))
         elif acao == 3:
-            return            
-       
+            index = dicionario[tipo].index(complex(entrada))
+            dicionario[tipo][index] = complex(input("Digite o novo dado\n"))
         elif acao == 4:
             dicionario[tipo].remove(complex(entrada))
     elif tipo == "list":
         if acao == 1:
             dicionario[tipo].append(list(entrada))
         elif acao == 3:
-            return            
-        
+            index = dicionario[tipo].index(list(entrada))
+            dicionario[tipo][index] = list(input("Digite o novo dado\n"))
         elif acao == 4:
             dicionario[tipo].remove(list(entrada))
     elif tipo == "tuple":
         if acao == 1:
             dicionario[tipo].append(tuple(entrada))
         elif acao == 3:
-            return            
-        
+            index = dicionario[tipo].index(tuple(entrada))
+            dicionario[tipo][index] = tuple(input("Digite o novo dado\n"))
         elif acao == 4:
             dicionario[tipo].remove(tuple(entrada))
     elif tipo == "range":
         if acao == 1:
             dicionario[tipo].append(range(entrada))
         elif acao == 3:
-            return           
-        
+            index = dicionario[tipo].index(range(entrada))
+            dicionario[tipo][index] = range(input("Digite o novo dado\n"))
         elif acao == 4:
             dicionario[tipo].remove(range(entrada))
     elif tipo == "str":
         if acao == 1:
             dicionario[tipo].append(str(entrada))
         elif acao == 3:
-            return            
-        
+            index = dicionario[tipo].index(str(entrada))
+            dicionario[tipo][index] = str(input("Digite o novo dado\n"))
         elif acao == 4:
             dicionario[tipo].remove(str(entrada))
     elif tipo == "bytes":
         if acao == 1:
             dicionario[tipo].append(bytes(entrada))
         elif acao == 3:
-            return            
-        
+            index = dicionario[tipo].index(bytes(entrada))
+            dicionario[tipo][index] = bytes(input("Digite o novo dado\n"))
         elif acao == 4:
             dicionario[tipo].remove(bytes(entrada))
     elif tipo == "bytearray":
         if acao == 1:
             dicionario[tipo].append(bytearray(entrada))
         elif acao == 3:
-            return            
-        
+            index = dicionario[tipo].index(bytearray(entrada))
+            dicionario[tipo][index] = bytearray(input("Digite o novo dado\n"))
         elif acao == 4:
             dicionario[tipo].remove(bytearray(entrada))
     elif tipo == "memoryview":
         if acao == 1:
             dicionario[tipo].append(memoryview(entrada))
         elif acao == 3:
-            return            
-        
+            index = dicionario[tipo].index(memoryview(entrada))
+            dicionario[tipo][index] = memoryview(input("Digite o novo dado\n"))
         elif acao == 4:
             dicionario[tipo].remove(memoryview(entrada))
     elif tipo == "set":
         if acao == 1:
             dicionario[tipo].append(set(entrada))
         elif acao == 3:
-            return            
-        
+            index = dicionario[tipo].index(set(entrada))
+            dicionario[tipo][index] = set(input("Digite o novo dado\n"))
         elif acao == 4:
             dicionario[tipo].remove(set(entrada))
     elif tipo == "frozenset":
         if acao == 1:
             dicionario[tipo].append(frozenset(entrada))
         elif acao == 3:
-            return            
-        
+            index = dicionario[tipo].index(frozenset(entrada))
+            dicionario[tipo][index] = frozenset(input("Digite o novo dado\n"))
         elif acao == 4:
             dicionario[tipo].remove(frozenset(entrada))
     elif tipo == "dict":
         if acao == 1:
             dicionario[tipo].append(dict(entrada))
         elif acao == 3:
-            return            
-        
+            index = dicionario[tipo].index(dict(entrada))
+            dicionario[tipo][index] = dict(input("Digite o novo dado\n"))
         elif acao == 4:
             dicionario[tipo].remove(dict(entrada))
 
