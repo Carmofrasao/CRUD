e032451dd08ad97456da5856185f14526dc383e8
Anderson Frasao <anderfrasao52@gmail.com>
2023-11-27 17:17:36 -0300

    assssssssssssssssss

--- CRUD.py
+++ CRUD.py
@@ -1,13 +1,33 @@
-
+import operator
 def Ler(dicionario):
+    max_key = max(dicionario.items(), key=operator.itemgetter(1))[0]
 
-    print("| Tipo   \t| Dado")
-    for tipo in dicionario:
-        print('|', tipo, '  \t| ', end="")
-        for dado in dicionario[tipo]:
-            print(dado, ' \t| ', end="")
+    for i in range(0, 2):
+        print(" ---------------", end="")
+    for i in range(0, dicionario[max_key][0]):
+        print(" ---------------", end="")
+    print()
+    print("| Tipo    \t| N. Itens    \t|", end="")
+    if dicionario[max_key][0] != 0:
+        print(" Dado    \t|")
+    else:
         print()
-
+    for i in range(0, 2):
+        print(" ---------------", end="")
+    for i in range(0, dicionario[max_key][0]):
+        print(" ---------------", end="")
+    print()
+    for tipo in dicionario:
+        print('|', tipo[:7], '    \t| ', end="")
+        for dado in dicionario[tipo]:
+            print(str(dado)[:6]+'..', '    \t| ', end="")
+        print()
+        for i in range(0, 2):
+            print(" ---------------", end="")
+        for i in range(0, dicionario[max_key][0]):
+            print(" ---------------", end="")
+        print()
+        
 def teste(tipo, dicionario):
 
     if tipo == "bool" or not tipo in dicionario:
@@ -21,6 +41,7 @@ def converter(dicionario, tipo, entrada, acao):
     if acao == 1:
         try:
             dicionario[tipo].append(type(eval(entrada))(entrada))
+            dicionario[tipo][0] += 1
         except:
             print("\nSintax invalida")
             return
@@ -34,6 +55,7 @@ def converter(dicionario, tipo, entrada, acao):
     elif acao == 4:
         try:
             dicionario[tipo].remove(type(eval(entrada))(entrada))
+            dicionario[tipo][0] -= 1
         except:
             print("\nSintax invalida")
             return
@@ -50,7 +72,7 @@ def acao():
 
 def main():
     # usando um dicionario para salvar as info
-    dicionario = {"int":[], "float":[], "complex":[], "list":[], "tuple":[], "range":[], "str":[], "list":[], "set":[], "frozenset":[], "dict":[]}
+    dicionario = {"int":[0,], "float":[0,], "complex":[0,], "list":[0,], "tuple":[0,], "range":[0,], "str":[0,], "list":[0,], "set":[0,], "frozenset":[0,], "dict":[0,]}
 
     print("Bem vindo")
 
